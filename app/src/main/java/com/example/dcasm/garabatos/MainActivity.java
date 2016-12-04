48eb436bd52790153411061a7c4a347585035112
dcasmor <dcasmor@outlook.com>
2016-12-05 00:12:45 +0100

    Stuff

--- app/src/main/java/com/example/dcasm/garabatos/MainActivity.java
+++ app/src/main/java/com/example/dcasm/garabatos/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.dcasm.garabatos;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -49,6 +44,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        drawingView = (DrawingView) findViewById(R.id.drawing);
     }
 
     @Override
